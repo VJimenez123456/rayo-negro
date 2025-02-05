086ac09d44f7a96146a9014183e07d86cfe0bbe2
Victor J <viktorjm10@gmail.com>
2025-02-05 13:34:11 -0500

    update id shopify sucursalID

--- app/apps/locations/helper.py
+++ app/apps/locations/helper.py
@@ -1,12 +1,12 @@
 # flake8: noqa
 sql_location_create = """
-    INSERT INTO locations (Location_name, location_shopify)
+    INSERT INTO locations (SucursalNombre, location_shopify)
     VALUES (%s, %s, %s)
 """
 sql_location_update = """
     INSERT INTO locations (SucursalID, SucursalNombre, location_shopify)
     VALUES (%s, %s, %s)
-    ON DUPLICATE KEY UPDATE Location_name=VALUES(Location_name), location_shopify=VALUES(location_shopify)
+    ON DUPLICATE KEY UPDATE SucursalNombre=VALUES(SucursalNombre), location_shopify=VALUES(location_shopify)
 """
 
 select_location = "SELECT * FROM locations WHERE location_shopify = %s"

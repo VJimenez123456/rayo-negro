16ce336fd2edc2add1d98acb2d772b41a550e142
Victor J <viktorjm10@gmail.com>
2025-02-05 13:28:42 -0500

    update id shopify

--- app/apps/locations/services.py
+++ app/apps/locations/services.py
@@ -28,7 +28,7 @@ async def update_location_service(location: LocationSchema):
     cursor = connection.cursor(dictionary=True)
     print("loc-shopify", location.id)
     is_updated = False
-    location_obj = (location.name, location.id)
+    location_obj = (location.id, location.name, location.id)
     print("location_obj-u", location_obj)
     try:
         cursor.execute(sql_location_update, location_obj)

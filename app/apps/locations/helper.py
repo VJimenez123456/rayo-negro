c89fc2ac8adee92e8f4064534e628f474bb1c780
Victor J <viktorjm10@gmail.com>
2025-02-05 12:31:22 -0500

    update locations name

--- app/apps/locations/helper.py
+++ app/apps/locations/helper.py
@@ -1,16 +1,16 @@
 # flake8: noqa
 sql_location_create = """
-    INSERT INTO location (Locatiosn_name, created_at, location_shopify)
+    INSERT INTO locations (Locatiosn_name, created_at, location_shopify)
     VALUES (%s, %s, %s)
 """
 sql_location_update = """
-    INSERT INTO location (id, Location_name, created_at, location_shopify)
+    INSERT INTO locations (id, Location_name, created_at, location_shopify)
     VALUES (%s, %s, %s)
     ON DUPLICATE KEY UPDATE Location_name=VALUES(Location_name), created_at=VALUES(created_at), location_shopify=VALUES(location_shopify)
 """
 
-select_location = "SELECT * FROM location WHERE location_shopify = %s"
+select_location = "SELECT * FROM locations WHERE location_shopify = %s"
 
-select_location_id = "SELECT id FROM location WHERE location_shopify = %s"
+select_location_id = "SELECT id FROM locations WHERE location_shopify = %s"
 
-delete_location = "DELETE FROM location WHERE location_shopify = %s"
+delete_location = "DELETE FROM locations WHERE location_shopify = %s"

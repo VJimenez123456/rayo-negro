f4c625ca4a4b5f6357230fae202669777a81f5a4
Victor J <viktorjm10@gmail.com>
2025-02-13 02:41:24 -0500

    comment routes

--- app/apps/system/routers.py
+++ app/apps/system/routers.py
@@ -13,17 +13,17 @@ from starlette.responses import JSONResponse
 router = APIRouter()
 
 
-@router.get("/inventory")
-async def update_inventory():
-    is_updated = await update_inventory_service()
-    if not is_updated:
-        JSONResponse(
-            {"message": "Error in update inventory"},
-            status_code=status.HTTP_400_BAD_REQUEST
-        )
-    return {
-        "message": "Successfully updated inventory",
-    }
+# @router.get("/inventory")
+# async def update_inventory():
+#     is_updated = await update_inventory_service()
+#     if not is_updated:
+#         JSONResponse(
+#             {"message": "Error in update inventory"},
+#             status_code=status.HTTP_400_BAD_REQUEST
+#         )
+#     return {
+#         "message": "Successfully updated inventory",
+#     }
 
 
 @router.get("/inventory_barcode")
@@ -65,14 +65,14 @@ async def update_inventory_barcode():
 #     }
 
 
-@router.get("/update_locations")
-async def update_locations_in_inventory():
-    is_updated = await update_locations_in_inventory_service()
-    if not is_updated:
-        JSONResponse(
-            {"message": "Error in update product"},
-            status_code=status.HTTP_400_BAD_REQUEST
-        )
-    return {
-        "message": "Successfully updated product",
-    }
+# @router.get("/update_locations")
+# async def update_locations_in_inventory():
+#     is_updated = await update_locations_in_inventory_service()
+#     if not is_updated:
+#         JSONResponse(
+#             {"message": "Error in update product"},
+#             status_code=status.HTTP_400_BAD_REQUEST
+#         )
+#     return {
+#         "message": "Successfully updated product",
+#     }

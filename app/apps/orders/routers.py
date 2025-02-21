9762a8694939cc8088d3332b76c95624feafb32d
Victor J <viktorjm10@gmail.com>
2025-02-21 13:15:26 -0500

    update_orders delete prinst

--- app/apps/orders/routers.py
+++ app/apps/orders/routers.py
@@ -21,11 +21,8 @@ router = APIRouter()
 
 
 @router.post("/update")
-async def update_order(order_dict: dict):
-    print("order_dict:", order_dict)
-    order = OrderSchema(**order_dict)
-    # print("order-obj:", order)
-    # print("order_id:", order.id)
+async def update_order(order: OrderSchema):
+    print("order_id:", order.id)
     is_updated = await update_order_service(order)
     if not is_updated:
         JSONResponse(

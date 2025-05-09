38cba012f7d869fb4f9935db91c3e5ae6d56aaf9
Victor J <viktorjm10@gmail.com>
2025-05-09 13:00:26 -0500

    orders schema 3

--- app/apps/orders/models.py
+++ app/apps/orders/models.py
@@ -23,21 +23,21 @@ class ShippingAddress(BaseModel):
 
 class Address(BaseModel):
     id: int
-    customer_id: int
-    first_name: str
-    last_name: str
+    customer_id: Optional[str] = None
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
     company: Optional[str] = None
-    address1: str
+    address1: Optional[str] = None
     address2: Optional[str] = None
-    city: str
-    province: str
-    country: str
-    zip: str
+    city: Optional[str] = None
+    province: Optional[str] = None
+    country: Optional[str] = None
+    zip: Optional[str] = None
     phone: Optional[str] = None
-    name: str
-    province_code: str
-    country_code: str
-    country_name: str
+    name: Optional[str] = None
+    province_code: Optional[str] = None
+    country_code: Optional[str] = None
+    country_name: Optional[str] = None
     default: bool
 
 

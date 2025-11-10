e8f2cfef91c093396822ffad8387e90264ac80a4
Victor Jiménez <viktorjm10@gmail.com>
2025-11-10 07:48:26 -0500

    update task

--- app/tasks.py
+++ app/tasks.py
@@ -7,8 +7,8 @@ logger = get_task_logger(__name__)
 
 @celery.task(name="app.tasks.daily.tarea_diaria", bind=True, max_retries=3)
 def tarea_diaria(self):
-    print("here")
-    logger.info("init")
-    synchronize_inventory_service()
-    logger.info("finish")
+    # print("here")
+    # logger.info("init")
+    # synchronize_inventory_service()
+    # logger.info("finish")
     return {"status": "ok"}
